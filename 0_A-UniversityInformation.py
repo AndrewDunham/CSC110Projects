0055bdf53cc2f2cfcd1d33481c4bb96a209a28dd
Andrew Dunham <37754965+AndrewDunham@users.noreply.github.com>
2018-10-25 12:30:55 -0700

    Create 0_A-UniversityInformation.py

---
+++ 0_A-UniversityInformation.py
@@ -0,0 +1,12 @@
+# Program 0-A:  University Information
+# Andrew Dunham
+
+def main():
+
+    #Simple program showing how printing works in python. 
+    
+    print('University Information:')
+    print('University of Victoria \n 3800 Finnerty Road \n Victoria BC V8P 5C2 \n CANADA')
+    
+if __name__ == '__main__':
+    main()
